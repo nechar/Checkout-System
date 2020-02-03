16f0cf32514d22b6460a57cb20fdc06f8b0c6be0
Nechar <joshinechar@gmail.com>
2020-02-03 11:16:43 +1100

    doc: remove unrelevant comments

--- src/Cart.ts
+++ src/Cart.ts
@@ -34,12 +34,6 @@ export class Cart {
       return cartItem[0];
     }
     return null;
-    /**
-     * Why did I use filter instead of find?
-     * Because for some reason, jest was unable to recognize find method
-     * Ideally I would have used the following if it worked
-     * return this.cartItems.find(item => item.sku === itemSKU)
-     */
   }
 
   getTotal(): number {
--- src/model/item.interface.ts
+++ src/model/item.interface.ts
@@ -1,18 +1,11 @@
-/**
- * Why do I prefer using `type` over `enum`?
- * Because, type has all the benefits, auto-complete and typechecking of `enum` plus the following:
- * 1. no need to export and import enums
- * 2. the code looks much readable
- * 3. the code looks much cleaner
- */
-type offerCode = "3for2" | "bulk-discount" | "freeItem" | null;
+type OfferCode = "3for2" | "bulk-discount" | "freeItem" | null;
 export type ItemSKU = "ipd" | "mbp" | "freeItem" | "atv" | "vga";
 
 export interface iItem {
   sku: ItemSKU;
   name: string;
   price: number;
-  offerCode: offerCode;
+  offerCode: OfferCode;
   quantity?: number;
   discountPrice?: number;
   freeItemSKU?: ItemSKU;
